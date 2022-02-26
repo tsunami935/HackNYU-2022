e41e39b2c1fbaf10ca010ecb77ef366e87f6ce81
Nam <ndbui03@gmail.com>
2022-02-25 23:16:24 -0500

    I can't code

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    #precondition: None
+    #postcondition: Sends user a welcome page and asks if they would like to disclose their location
+    #   If yes, send a request to EPA AQS API with relevant information
+    return render_template("welcome.html")
+
+@app.route("/pollution-report")
+def pollution_report():
+    return send_from_directory("templates", "success.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ app/static/welcome.js
@@ -0,0 +1,5 @@
+document.onload = function() {
+    document.getElementById("enter_button").onclick = function(){
+        location.href = "/pollution-report";
+    };
+};//i can't code

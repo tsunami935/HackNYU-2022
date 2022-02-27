15bde49dece9e1731ea2d23897971761b46ebfd1
Nam <ndbui03@gmail.com>
2022-02-27 00:11:00 -0500

    moved table constructor to its own function

--- app/static/js/pollution-report.js
+++ app/static/js/pollution-report.js
@@ -14,25 +14,29 @@ function sendPosition(position){
                     console.log("/get-report returned no results");
                     return;
                 }
-                //create and populate table
-                document.getElementById("report-placeholder").remove();
-                var table = document.createElement("table");
-                table.style.border = '1px solid black';
-                const header = table.insertRow();
-                header.insertCell().appendChild(document.createTextNode("Pollutant"));
-                header.insertCell().appendChild(document.createTextNode("Rating"));
-                for(let i = 0; i < data.length; i++){
-                    const row = table.insertRow();
-                    const pollutant = row.insertCell();
-                    pollutant.appendChild(document.createTextNode(data[i].pollutant));
-                    pollutant.style.border = '1px solid black';
-                    const aqi = row.insertCell();
-                    aqi.style.border = '1px solid black';
-                    aqi.appendChild(document.createTextNode(aqi_rating(data[i].aqi)));
-                }
-                document.getElementById("container").appendChild(table);
+                create_report_table(data);
             }
         )
+}
+
+function create_report_table(data){
+    //create and populate table
+    document.getElementById("report-placeholder").remove();
+    var table = document.createElement("table");
+    table.style.border = '1px solid black';
+    const header = table.insertRow();
+    header.insertCell().appendChild(document.createTextNode("Pollutant"));
+    header.insertCell().appendChild(document.createTextNode("Rating"));
+    for(let i = 0; i < data.length; i++){
+        const row = table.insertRow();
+        const pollutant = row.insertCell();
+        pollutant.appendChild(document.createTextNode(data[i].pollutant));
+        pollutant.style.border = '1px solid black';
+        const aqi = row.insertCell();
+        aqi.style.border = '1px solid black';
+        aqi.appendChild(document.createTextNode(aqi_rating(data[i].aqi)));
+    }
+    document.getElementById("container").appendChild(table);
 }
 
 function aqi_rating(aqi){
